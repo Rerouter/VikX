c16e6b9b1ad3e5ad11294714ff3c4eae5a245eee
DogScopeAZ <102510778+DogScopeAZ@users.noreply.github.com>
2023-06-13 12:04:48 -0700

    Update closer_dog.py
    added some outputs, appears to hang with larger image sets. Also removed picture size limits.

--- closer_dog.py
+++ closer_dog.py
@@ -3,31 +3,32 @@ from tensorflow import keras
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import LeakyReLU, PReLU, ELU, Multiply, Dense, Reshape, GlobalAveragePooling2D, Input, Conv2DTranspose, Conv2D, BatchNormalization, Dropout, MaxPooling2D,Concatenate,UpSampling2D,Add
 from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 import cv2
+from datetime import datetime
 from skimage.metrics import structural_similarity as ssim
 from skimage.metrics import mean_squared_error
 from scipy.ndimage import gaussian_gradient_magnitude
 import numpy as np
 from scipy.optimize import curve_fit
 from tensorflow.signal import fft2d, ifft2d, fftshift
-from datetime import datetime
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+from PIL import Image
 
-#Hyper parameters
-batch_size = 256
-epochz = 400
-learning_rate=0.001
+Image.MAX_IMAGE_PIXELS = None  # No limit to image size
+#Hyper
 # data directories
-#todo get these easier to map
+batch_size = 16
+epochz = 100
+learning_rate=0.001
 data_directory_path = "Z:\\AstroImageAI\\"
 data_dir_train = "Z:\\AstroImageAI\\TrainData"  # make sure to use correct slashes for directory paths
 data_dir_validation = "Z:\\AstroImageAI\\Validation"
 
 # minimum size
-min_width = 256  # you can change this to your preferred minimum width
-min_height = 256  # you can change this to your preferred minimum height
+min_width = 1000
+min_height = 625
 
 # for grayscale or RGB images
 color_mode = "rgb"  # you can change this to 'grayscale' if you want to handle grayscale images
@@ -71,6 +72,7 @@ def pad_images(images):
     return np.array(padded_images)
 
 # load images
+print("Loading training images...")
 train_images = []
 train_shapes = []  # store the original shapes of the images
 for filename in os.listdir(data_dir_train):
@@ -79,7 +81,9 @@ for filename in os.listdir(data_dir_train):
     im = preprocess_image(im)
     train_images.append(im)
     train_shapes.append(im_shape)
+print("Training images loaded.")
 
+print("Loading validation images...")
 val_images = []
 val_shapes = []  # store the original shapes of the images
 for filename in os.listdir(data_dir_validation):
@@ -88,6 +92,7 @@ for filename in os.listdir(data_dir_validation):
     im = preprocess_image(im)
     val_images.append(im)
     val_shapes.append(im_shape)
+print("Validation images loaded.")
 
 # convert lists to numpy arrays
 train_images = np.array(train_images)
@@ -100,28 +105,41 @@ val_images = pad_images(val_images)
 # normalize images
 train_images = train_images / 255.0
 val_images = val_images / 255.0
-
-# Create an image data generator object
-datagen = ImageDataGenerator(
-    rotation_range=180,
-    width_shift_range=0.5,
-    height_shift_range=0.5,
-    shear_range=0.6,
-    zoom_range=0.6,
-    horizontal_flip=True,
-    fill_mode='nearest')
-# fit parameters from data
-datagen.fit(train_images)
-
-
 #define model
 def squeeze_excite_block(input, ratio=16):
     channels = input.shape[-1]
     se = GlobalAveragePooling2D()(input)
     se = Reshape((1, 1, channels))(se)
-    se = Dense(channels // ratio, activation='relu')(se)
-    se = Dense(channels, activation='sigmoid')(se)
+
+    # Add a Convolutional layer to capture spatial information
+    se = Conv2D(channels // ratio, kernel_size=1, activation='relu')(se)
+
+    # Modify the attention mechanism to focus on nebulae
+    se = Conv2D(channels, kernel_size=1, activation='sigmoid')(se)
+
     return Multiply()([input, se])
+def star_attention_block(input, ratio=16):
+    channels = input.shape[-1]
+    se = GlobalAveragePooling2D()(input)
+    se = Reshape((1, 1, channels))(se)
+
+    # Add a Convolutional layer to capture spatial information
+    se = Conv2D(channels // ratio, kernel_size=1, activation='relu')(se)
+
+    # Modify the attention mechanism to focus on stars
+    se = Conv2D(channels, kernel_size=1, activation='sigmoid')(se)
+
+    return Multiply()([input, se])
+datagen = ImageDataGenerator(
+    rotation_range=359,
+    width_shift_range=0,
+    height_shift_range=0,
+    shear_range=0.1,
+    zoom_range=0.1,
+    horizontal_flip=True,
+    fill_mode='nearest')
+# fit parameters from data
+datagen.fit(train_images)
 num_channels = 1 if color_mode == 'grayscale' else 3
 inputs = Input(shape=(None, None, num_channels))
 alphaV=0.1
@@ -132,81 +150,143 @@ conv1 = LeakyReLU(alpha=alphaV)(conv1)
 conv2 = Conv2D(64, 3, padding='same', dilation_rate=2)(conv1)
 conv2 = squeeze_excite_block(conv2)
 conv2 = LeakyReLU(alpha=alphaV)(conv2)
+conv2 = Conv2D(64, 3, padding='same')(conv2)
+conv2 = LeakyReLU(alpha=alphaV)(conv2)
 pool1 = MaxPooling2D(pool_size=(2, 2))(conv2)
 
 skip1 = conv2
 
 conv3 = Conv2D(128, 3, padding='same')(pool1)
+conv3 = squeeze_excite_block(conv3)
+conv3 = LeakyReLU(alpha=alphaV)(conv3)
+conv3 = Conv2D(128, 3, padding='same')(conv3)
 conv3 = LeakyReLU(alpha=alphaV)(conv3)
 
 conv4 = Conv2D(128, 3, padding='same')(conv3)
+conv4 = LeakyReLU(alpha=alphaV)(conv4)
+conv4 = Conv2D(128, 3, padding='same')(conv4)
 conv4 = LeakyReLU(alpha=alphaV)(conv4)
 pool2 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
 conv5 = Conv2D(256, 3, padding='same')(pool2)
 conv5 = LeakyReLU(alpha=alphaV)(conv5)
+conv5 = Conv2D(256, 3, padding='same')(conv5)
+conv5 = LeakyReLU(alpha=alphaV)(conv5)
 
 conv6 = Conv2D(256, 3, padding='same')(conv5)
 conv6 = LeakyReLU(alpha=alphaV)(conv6)
+conv6 = Conv2D(256, 3, padding='same')(conv6)
+conv6 = LeakyReLU(alpha=alphaV)(conv6)
 
 conv7 = Conv2D(256, 3, padding='same', dilation_rate=2)(conv6)
+conv7 = LeakyReLU(alpha=alphaV)(conv7)
+conv7 = Conv2D(256, 3, padding='same')(conv7)
 conv7 = LeakyReLU(alpha=alphaV)(conv7)
 pool3 = MaxPooling2D(pool_size=(2, 2))(conv7)
 
 conv8 = Conv2D(512, 3, padding='same')(pool3)
 conv8 = LeakyReLU(alpha=alphaV)(conv8)
+conv8 = Conv2D(512, 3, padding='same')(conv8)
+conv8 = LeakyReLU(alpha=alphaV)(conv8)
 
 conv9 = Conv2D(512, 3, padding='same')(conv8)
 conv9 = LeakyReLU(alpha=alphaV)(conv9)
+conv9 = Conv2D(512, 3, padding='same')(conv9)
+conv9 = LeakyReLU(alpha=alphaV)(conv9)
 
 conv10 = Conv2D(512, 3, padding='same')(conv9)
+conv10 = LeakyReLU(alpha=alphaV)(conv10)
+conv10 = Conv2D(512, 3, padding='same')(conv10)
 conv10 = LeakyReLU(alpha=alphaV)(conv10)
 pool4 = MaxPooling2D(pool_size=(2, 2))(conv10)
 
 # Bottleneck
 conv11 = Conv2D(1024, 3, padding='same')(pool4)
 conv11 = LeakyReLU(alpha=alphaV)(conv11)
-
-conv12 = Conv2D(1024, 3, padding='same')(conv11)
-conv12 = LeakyReLU(alpha=alphaV)(conv12)
+conv11 = Conv2D(1024, 3, padding='same')(conv11)
+conv11 = LeakyReLU(alpha=alphaV)(conv11)
 
 # Decoder
-upsample1 = UpSampling2D((2, 2))(conv12)
+upsample1 = UpSampling2D((2, 2))(conv11)
+upsample1 = squeeze_excite_block(upsample1)
+upsample1 = Conv2D(512, 3, padding='same')(upsample1)
+upsample1 = LeakyReLU(alpha=alphaV)(upsample1)
+upsample1 = Conv2D(512, 3, padding='same')(upsample1)
+upsample1 = LeakyReLU(alpha=alphaV)(upsample1)
+upsample1 = tf.image.resize(upsample1, tf.shape(conv10)[1:3])
 concat1 = Concatenate()([upsample1, conv10])
 conv13 = Conv2D(512, 3, padding='same')(concat1)
+conv13 = LeakyReLU(alpha=alphaV)(conv13)
+conv13 = Conv2D(512, 3, padding='same')(conv13)
 conv13 = LeakyReLU(alpha=alphaV)(conv13)
 
 conv14 = Conv2D(512, 3, padding='same', dilation_rate=2)(conv13)
 conv14 = LeakyReLU(alpha=alphaV)(conv14)
+conv14 = Conv2D(512, 3, padding='same')(conv14)
+conv14 = LeakyReLU(alpha=alphaV)(conv14)
 
 conv15 = Conv2D(512, 3, padding='same')(conv14)
 conv15 = LeakyReLU(alpha=alphaV)(conv15)
+conv15 = Conv2D(512, 3, padding='same')(conv15)
+conv15 = LeakyReLU(alpha=alphaV)(conv15)
 
 upsample2 = UpSampling2D((2, 2))(conv15)
+upsample2 = star_attention_block(upsample2)
+upsample2 = Conv2D(256, 3, padding='same')(upsample2)
+upsample2 = LeakyReLU(alpha=alphaV)(upsample2)
+upsample2 = Conv2D(256, 3, padding='same')(upsample2)
+upsample2 = LeakyReLU(alpha=alphaV)(upsample2)
+upsample2 = tf.image.resize(upsample2, tf.shape(conv7)[1:3])
 concat2 = Concatenate()([upsample2, conv7])
 conv16 = Conv2D(256, 3, padding='same')(concat2)
+conv16 = LeakyReLU(alpha=alphaV)(conv16)
+conv16 = Conv2D(256, 3, padding='same')(conv16)
 conv16 = LeakyReLU(alpha=alphaV)(conv16)
 
 conv17 = Conv2D(256, 3, padding='same', dilation_rate=2)(conv16)
 conv17 = LeakyReLU(alpha=alphaV)(conv17)
+conv17 = Conv2D(256, 3, padding='same')(conv17)
+conv17 = LeakyReLU(alpha=alphaV)(conv17)
 
 conv18 = Conv2D(256, 3, padding='same')(conv17)
 conv18 = LeakyReLU(alpha=alphaV)(conv18)
+conv18 = Conv2D(256, 3, padding='same')(conv18)
+conv18 = LeakyReLU(alpha=alphaV)(conv18)
 
 upsample3 = UpSampling2D((2, 2))(conv18)
+upsample3 = squeeze_excite_block(upsample3)
+upsample3 = Conv2D(128, 3, padding='same')(upsample3)
+upsample3 = LeakyReLU(alpha=alphaV)(upsample3)
+upsample3 = Conv2D(128, 3, padding='same')(upsample3)
+upsample3 = LeakyReLU(alpha=alphaV)(upsample3)
+upsample3 = tf.image.resize(upsample3, tf.shape(conv4)[1:3])
 concat3 = Concatenate()([upsample3, conv4])
 conv19 = Conv2D(128, 3, padding='same')(concat3)
+conv19 = LeakyReLU(alpha=alphaV)(conv19)
+conv19 = Conv2D(128, 3, padding='same')(conv19)
 conv19 = LeakyReLU(alpha=alphaV)(conv19)
 
 conv20 = Conv2D(128, 3, padding='same')(conv19)
 conv20 = LeakyReLU(alpha=alphaV)(conv20)
+conv20 = Conv2D(128, 3, padding='same')(conv20)
+conv20 = LeakyReLU(alpha=alphaV)(conv20)
 
 upsample4 = UpSampling2D((2, 2))(conv20)
+upsample4 = star_attention_block(upsample4)
+upsample4 = Conv2D(64, 3, padding='same')(upsample4)
+upsample4 = LeakyReLU(alpha=alphaV)(upsample4)
+upsample4 = Conv2D(64, 3, padding='same')(upsample4)
+upsample4 = LeakyReLU(alpha=alphaV)(upsample4)
+upsample4 = tf.image.resize(upsample4, tf.shape(conv2)[1:3])
 concat4 = Concatenate()([upsample4, conv2])
 conv21 = Conv2D(64, 3, padding='same')(concat4)
 conv21 = LeakyReLU(alpha=alphaV)(conv21)
+conv21 = Conv2D(64, 3, padding='same')(conv21)
+conv21 = LeakyReLU(alpha=alphaV)(conv21)
 
 conv22 = Conv2D(64, 3, padding='same', dilation_rate=3)(conv21)
+conv22 = LeakyReLU(alpha=alphaV)(conv22)
+conv22 = Conv2D(64, 3, padding='same')(conv22)
 conv22 = LeakyReLU(alpha=alphaV)(conv22)
 
 # Residual connection
@@ -218,8 +298,6 @@ model = Model(inputs=inputs, outputs=output)
 
 # print the model summary
 model.summary()
-
-# get the current date and time
 now = datetime.now()
 
 # format this into a string
@@ -235,10 +313,10 @@ model.save(data_directory_path+filename)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
 model.compile(optimizer=optimizer, loss='mse')
-history = model.fit(datagen.flow(train_images, train_images, batch_size=batch_size), 
-                    validation_data=(val_images, val_images), 
+history = model.fit(datagen.flow(train_images, train_images, batch_size=batch_size),
+                    validation_data=(val_images, val_images),
                     epochs=epochz)
-sample_image_path = r"Z:\\AstroImageAI\\Validation\\edit2_starless.tif"
+sample_image_path = r"Z:\\AstroImageAI\\Validation\\Velatestedit.tif"
 sample_image = Image.open(sample_image_path)
 sample_image = preprocess_image_2(sample_image)
 sample_image = sample_image / 255.0
@@ -246,7 +324,7 @@ sample_image = np.expand_dims(sample_image, axis=0)
 output_image = model.predict(sample_image)
 output_image = output_image.squeeze() * 255.0
 output_image = Image.fromarray(output_image.astype(np.uint8))
-# Define the file path
+output_image.show()
 file_path = f"Z:\\AstroImageAI\\output_image_{date_time}.png"
 
 # Save the image
